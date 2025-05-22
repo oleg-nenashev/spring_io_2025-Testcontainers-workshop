f7a3e541e3ef39d2becf610d6090eb63e984592e
Oleg Nenashev <onenashev@gradle.com>
2025-05-22 12:46:22 +0200

    Fix the merge conflict

--- src/test/java/com/example/todos/ContainersConfig.java
+++ src/test/java/com/example/todos/ContainersConfig.java
@@ -4,7 +4,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistrar;
-
+import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.DockerModelRunnerContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.wiremock.integrations.testcontainers.WireMockContainer;
@@ -20,9 +20,17 @@ public class ContainersConfig {
     }
 
     @Bean
-    WireMockContainer wireMockContainer(){
-        var container = new WireMockContainer("wiremock/wiremock:3.1.0")
+    WireMockContainer wireMockContainer() {
+        var container = new WireMockContainer("wiremock/wiremock:3.13.0")
             .withMappingFromResource("hackernews", "mappings/hackernews_v0-stubs.json");
+
+        return container;
+    }
+
+    @Bean
+    WireMockContainer mockedModelRunner(DynamicPropertyRegistry properties){
+        var container = new WireMockContainer("wiremock/wiremock:3.13.0")
+                .withMappingFromResource("AI Model", "ai/mappings/aimodel.json");
 
         return container;
     }
